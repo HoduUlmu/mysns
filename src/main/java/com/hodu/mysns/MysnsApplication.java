cfcd18c80a7f2880067a8a4e06c9818e39e317d4
HoduUlmu <timewarp96@naver.com>
2022-09-18 08:48:28 +0900

    feat : add front-end code and modify build.gradle, add deploy.yml

--- src/main/java/com/hodu/mysns/MysnsApplication.java
+++ src/main/java/com/hodu/mysns/MysnsApplication.java
@@ -2,8 +2,9 @@ package com.hodu.mysns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class MysnsApplication {
 
 	public static void main(String[] args) {
