0a0120440e30568ef219d0559ccb998dd19e54a1
HoduUlmu <timewarp96@naver.com>
2022-09-18 08:11:00 +0900

    init : initialize project

---
+++ src/test/java/com/hodu/mysns/MysnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hodu.mysns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
